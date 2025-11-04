dcef3c0631369979a701d29407165f4228307b55
oliver <omiguezalonso@danielcastelao.org>
2025-11-04 13:13:30 +0100

    Borrado Importacion Innecesaria

--- app/src/main/java/gz/dam/simondicejorgeoliver/MainActivity.kt
+++ app/src/main/java/gz/dam/simondicejorgeoliver/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.ViewModel
 import gz.dam.simondicejorgeoliver.ui.theme.SimonDiceJorgeOliverTheme
 
 class MainActivity : ComponentActivity() {
