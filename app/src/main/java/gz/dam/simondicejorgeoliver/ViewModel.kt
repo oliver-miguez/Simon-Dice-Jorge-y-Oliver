dabf78b142d576bae1c7172fe8a923d0b7577f18
oliver <omiguezalonso@danielcastelao.org>
2025-11-04 11:36:21 +0100

    ViewModel: Generar un número aleatorio

--- app/src/main/java/gz/dam/simondicejorgeoliver/ViewModel.kt
+++ app/src/main/java/gz/dam/simondicejorgeoliver/ViewModel.kt
@@ -5,10 +5,12 @@ import kotlinx.coroutines.flow.MutableStateFlow
 
 class ViewModel(){
     val estadoActual: MutableStateFlow<Estados> = MutableStateFlow(Estados.INICIO)
-
+    var numeroRandomGenerado = MutableStateFlow(0)
 
     fun numeroRandom(){
         estadoActual.value = Estados.GENERANDO
         Log.d("ViewModel","Estado generando")
+        numeroRandomGenerado.value = (0..3).random()
+        Log.d("ViewModel","Número aleatorio generado: $numeroRandomGenerado")
     }
 }
