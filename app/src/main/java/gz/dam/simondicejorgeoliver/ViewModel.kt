83aa2f0400bfc7a99c6d8e2804eb37ff3fa01a93
oliver <omiguezalonso@danielcastelao.org>
2025-11-04 11:33:13 +0100

    ViewModel: Creando clase Modificando estado de generación de número aleatorio

--- app/src/main/java/gz/dam/simondicejorgeoliver/ViewModel.kt
+++ app/src/main/java/gz/dam/simondicejorgeoliver/ViewModel.kt
@@ -1,2 +1,14 @@
 package gz.dam.simondicejorgeoliver
 
+import android.util.Log
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class ViewModel(){
+    val estadoActual: MutableStateFlow<Estados> = MutableStateFlow(Estados.INICIO)
+
+
+    fun numeroRandom(){
+        estadoActual.value = Estados.GENERANDO
+        Log.d("ViewModel","Estado generando")
+    }
+}
