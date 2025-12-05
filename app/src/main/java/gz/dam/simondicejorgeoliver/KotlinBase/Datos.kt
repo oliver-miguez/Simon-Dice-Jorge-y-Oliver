19239cc8294533391a39bec3434bc4f95e79c173
oliver <omiguezalonso@danielcastelao.org>
2025-12-05 12:38:06 +0100

    pruebas de test en githug

--- app/src/main/java/gz/dam/simondicejorgeoliver/KotlinBase/Datos.kt
+++ app/src/main/java/gz/dam/simondicejorgeoliver/KotlinBase/Datos.kt
@@ -4,7 +4,6 @@ import androidx.compose.ui.graphics.Color
 
 object Datos {
     var numero: ArrayList<Int> = ArrayList()
-
 }
 
 enum class Colores(val color: Color, val txt: String, val nota: Double) {
